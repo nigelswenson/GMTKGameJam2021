84df15ac59e659ea7294ef11237b7847374a9a08
bmackenzie <brian.e.mackenzie@outlook.com>
2021-06-11 18:26:03 -0500

    Finished Dialogue System
    Moved trigger functionality into Dialogue Manager script that triggers on start

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+	[SerializeField] Text nameText;
+	[SerializeField] Text dialogueText;
+	[SerializeField] Dialogue[] dialogues;
+	public int dialogueIndex = 0;
+
+	[SerializeField] Animator animator;
+	[SerializeField] float textSpeed = .02f;
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start()
+	{
+		sentences = new Queue<string>();
+		animator.SetBool("IsOpen", true);
+		StartDialogue(dialogues[dialogueIndex]);
+	}
+
+	public void StartDialogue(Dialogue dialogue)
+	{
+
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence()
+	{
+		if (sentences.Count == 0)
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence(string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return new WaitForSeconds(textSpeed);
+		}
+	}
+
+	void EndDialogue()
+	{
+		dialogueIndex++;
+		//animator.SetBool("IsOpen", false);
+		if(dialogueIndex <= dialogues.Length)
+		{
+			StartDialogue(dialogues[dialogueIndex]);
+		}
+	}
+
+}
